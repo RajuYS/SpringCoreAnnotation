4ff2d105fcf528e270a7faa1c10116170413449d
RajuP <peddiraju.y6@gmail.com>
2017-03-09 18:38:14 +0530

    Property files readingse

--- src/main/java/com/spring/core/App.java
+++ src/main/java/com/spring/core/App.java
@@ -9,12 +9,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
 
-///*//        ApplicationContext context1=new ClassPathXmlApplicationContext("AppConfig.class");*/
+        /*ApplicationContext context1=new ClassPathXmlApplicationContext("AppConfig.class");
         ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
         Student st= (Student) context.getBean("std");
         st.display();
@@ -22,6 +22,12 @@ public class App
         Education ed= (Education) context.getBean("edu");
         System.out.println("From Education Class1"+ed);
         Education ed2= (Education) context.getBean("education");
-        System.out.println("From Education Class2"+ed2);
+        System.out.println("From Education Class2"+ed2);*/
+
+        ApplicationContext context=new AnnotationConfigApplicationContext(AppConfig.class);
+        DbProperties db = (DbProperties) context.getBean("db");
+        System.out.println(db.toString());
+
+
     }
 }
